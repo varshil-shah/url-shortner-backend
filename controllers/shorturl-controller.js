b0559307b165140757d8b3158b8222b2f2c94da7
Varshil-Shah <varshilashah09@gmail.com>
2023-03-18 14:30:36 +0530

    :sparkles: Implement a route to delete short url

--- controllers/shorturl-controller.js
+++ controllers/shorturl-controller.js
@@ -1,9 +1,34 @@
 const crc32 = require("crc-32");
 
 const ShortUrl = require("../models/shorturl-model");
-const catchAsync = require("../utils/catch-async");
 const AppError = require("../utils/app-error");
+const Analytics = require("../models/analytics-model");
+
+const catchAsync = require("../utils/catch-async");
 const StatusCode = require("../utils/status-code");
+
+exports.restrictDeleteShortUrl = catchAsync(async (req, res, next) => {
+  const { shortCode } = req.params;
+  const shortUrlInstance = await ShortUrl.findOne({
+    shortCode,
+    userId: req.user._id,
+  });
+
+  if (!shortUrlInstance) {
+    return next(
+      new AppError("No url found with this short code.", StatusCode.NOT_FOUND)
+    );
+  } else if (shortUrlInstance.userId !== req.user._id) {
+    return next(
+      new AppError(
+        "You don't have access to delete this url.",
+        StatusCode.FORBIDDEN
+      )
+    );
+  }
+
+  next();
+});
 
 exports.createShortUrl = catchAsync(async (req, res, next) => {
   // Get url from req.body
@@ -45,4 +70,34 @@ exports.createShortUrl = catchAsync(async (req, res, next) => {
 exports.redirectShortUrl = catchAsync(async (req, res, next) => {
   // Redirect user
   res.status(StatusCode.MOVED_TEMPORARILY).redirect(req.longUrl);
+});
+
+// Update the status of shorturl from active to inactive
+exports.deleteShortUrl = catchAsync(async (req, res, next) => {
+  const { shortCode } = req.params;
+  const shortUrlInstance = await ShortUrl.findOneAndDelete({ shortCode });
+
+  if (!shortUrlInstance) {
+    return next(
+      new AppError(
+        "No short url found with that short code.",
+        StatusCode.NOT_FOUND
+      )
+    );
+  }
+
+  const analyticsInstance = await Analytics.deleteMany({ shortCode });
+  if (!analyticsInstance) {
+    return next(
+      new AppError(
+        "Something went wrong while delete analytics log.",
+        StatusCode.NOT_FOUND
+      )
+    );
+  }
+
+  res.status(StatusCode.NO_CONTENT).json({
+    status: "success",
+    data: null,
+  });
 });
