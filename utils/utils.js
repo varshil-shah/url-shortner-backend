c326c684a7da356075f3fd6bb55758cd79551369
Varshil-Shah <varshilashah09@gmail.com>
2023-03-22 10:33:06 +0530

    :recycle: Create a func to return formated date

--- utils/utils.js
+++ utils/utils.js
@@ -4,3 +4,22 @@
  * @returns {String}
  */
 exports.extractString = (value) => value.replace(/[0-9.]/g, "");
+
+/**
+ * Format date in string
+ * @param {Number} next use to increment the no. of days
+ * @param {Date} date
+ * @returns {string} returns a formatted date in string
+ */
+exports.formatDate = (next = 0, date = new Date()) => {
+  if (next) date.setDate(date.getDate() + next);
+
+  // Get year, month, and day part from the date
+  const year = date.toLocaleString("default", { year: "numeric" });
+  const month = date.toLocaleString("default", { month: "2-digit" });
+  const day = date.toLocaleString("default", { day: "2-digit" });
+
+  // Generate yyyy-mm-dd date string
+  const formattedDate = year + "-" + month + "-" + day;
+  return formattedDate;
+};
