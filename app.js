a03d17dae8b3ec337429aab28a1a1270e20f1ea9
Varshil Shah <varshilshah.in@gmail.com>
2023-10-24 12:25:00 +0530

    Add pnpm lockfile and email scope for github

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ passport.use(
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackURL: process.env.GITHUB_CALLBACK_URL,
+      scope: ["user:email"],
     },
     async function (accessToken, refreshToken, profile, done) {
       try {
